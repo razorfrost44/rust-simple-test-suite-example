26b9174d003fa15ae471b5c3430ef07b9a82d1aa
Raymond Gottsch <rgottsch@logitech.com>
2025-04-16 10:45:10 -0700

    Put tests in same files as modules

--- src/grocery_store.rs
+++ src/grocery_store.rs
@@ -1,6 +1,4 @@
 // Imports - Tests
-#[cfg(test)]
-mod tests;
 use mockall::automock;
 // Imports - Rust
 use std::sync::Mutex;
@@ -68,5 +66,74 @@ impl GroceryStore for GroceryStoreImpl {
     fn set_current_vegetable(&self, vegetable: Vegetable) {
         let mut vegetable_global = self.current_vegetable.lock().unwrap();
         *vegetable_global = vegetable.clone();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestContext {
+        grocery_store: GroceryStoreImpl,
+    }
+
+    // Implementing any special needs for when TestContext is destroyed
+    impl Drop for TestContext {
+        fn drop(&mut self) {
+            // Clean up any resources if needed
+        }
+    }
+
+    fn setup() -> TestContext {
+        TestContext {
+            grocery_store: GroceryStoreImpl::new(),
+        }
+    }
+
+    #[test]
+    fn test_current_fruit() {
+        // Arrange
+        let context = setup();
+        // Act
+        let result = context.grocery_store.get_current_fruit();
+        // Assert
+        assert_eq!(result, Fruit::Apple);
+    }
+
+    #[test]
+    fn test_set_current_fruit() {
+        // Arrange
+        let context = setup();
+        context.grocery_store.set_current_fruit(Fruit::Banana);
+        // Act
+        let result = context.grocery_store.get_current_fruit();
+        // Assert
+        assert_eq!(result, Fruit::Banana);
+    }
+
+    #[test]
+    fn test_current_vegetable() {
+        // Arrange
+        let context = setup();
+        context
+            .grocery_store
+            .set_current_vegetable(Vegetable::Carrot);
+        // Act
+        let result = context.grocery_store.get_current_vegetable();
+        // Assert
+        assert_eq!(result, Vegetable::Carrot);
+    }
+
+    #[test]
+    fn test_set_current_vegetable() {
+        // Arrange
+        let context = setup();
+        context
+            .grocery_store
+            .set_current_vegetable(Vegetable::Spinach);
+        // Act
+        let result = context.grocery_store.get_current_vegetable();
+        // Assert
+        assert_eq!(result, Vegetable::Spinach);
     }
 }
--- src/shopper.rs
+++ src/shopper.rs
@@ -1,6 +1,3 @@
-// Imports - Tests
-#[cfg(test)]
-mod tests;
 // Imports - modules
 use crate::grocery_store;
 use grocery_store::{Fruit, GroceryStore, Vegetable};
@@ -33,4 +30,118 @@ pub fn grab_fruit_and_vegetable(store: &dyn GroceryStore) -> String {
     let vegetable = grab_vegetable(store);
 
     format!("{} {}", fruit, vegetable)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::grocery_store::MockGroceryStore;
+
+    struct TestContext {
+        grocery_store: MockGroceryStore,
+    }
+
+    // Implementing any special needs for when TestContext is destroyed
+    impl Drop for TestContext {
+        fn drop(&mut self) {
+            // Clean up any resources if needed
+        }
+    }
+
+    fn setup() -> TestContext {
+        TestContext {
+            grocery_store: MockGroceryStore::new(),
+        }
+    }
+
+    #[test]
+    fn test_grab_fruit() {
+        // Arrange
+        let mut context = setup();
+        context
+            .grocery_store
+            .expect_get_current_fruit()
+            .return_const(Fruit::Apple);
+        let expect = String::from("Apple");
+        // Act
+        let result = grab_fruit(&context.grocery_store);
+        // Assert
+        assert_eq!(result, expect);
+    }
+
+    #[test]
+    fn test_change_fruit() {
+        // Arrange
+        let mut context = setup();
+        context
+            .grocery_store
+            .expect_set_current_fruit()
+            .withf(|fruit| *fruit == Fruit::Banana)
+            .return_const(());
+        context
+            .grocery_store
+            .expect_get_current_fruit()
+            .return_const(Fruit::Banana);
+        // Act
+        change_fruit(&context.grocery_store, Fruit::Banana);
+        let result = grab_fruit(&context.grocery_store);
+        let expect = String::from("Banana");
+        // Assert
+        assert_eq!(result, expect);
+    }
+
+    #[test]
+    fn test_grab_vegetable() {
+        // Arrange
+        let mut context = setup();
+        context
+            .grocery_store
+            .expect_get_current_vegetable()
+            .return_const(Vegetable::Carrot);
+        // Act
+        let result = grab_vegetable(&context.grocery_store);
+        let expect = String::from("Carrot");
+        // Assert
+        assert_eq!(result, expect);
+    }
+
+    #[test]
+    fn test_change_vegetable() {
+        // Arrange
+        let mut context = setup();
+        context
+            .grocery_store
+            .expect_set_current_vegetable()
+            .withf(|vegetable| *vegetable == Vegetable::Broccoli)
+            .return_const(());
+        context
+            .grocery_store
+            .expect_get_current_vegetable()
+            .return_const(Vegetable::Broccoli);
+        // Act
+        change_vegetable(&context.grocery_store, Vegetable::Broccoli);
+        let result = grab_vegetable(&context.grocery_store);
+        let expect = String::from("Broccoli");
+        // Assert
+        assert_eq!(result, expect);
+    }
+
+    #[test]
+    fn test_grab_fruit_and_vegetable() {
+        // Arrange
+        let mut context = setup();
+        context
+            .grocery_store
+            .expect_get_current_fruit()
+            .return_const(Fruit::Apple);
+        context
+            .grocery_store
+            .expect_get_current_vegetable()
+            .return_const(Vegetable::Carrot);
+        // Act
+        let result = grab_fruit_and_vegetable(&context.grocery_store);
+        let expect = String::from("Apple Carrot");
+        // Assert
+        assert_eq!(result, expect);
+    }
 }
